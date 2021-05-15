1ceb9a520b9b6c7bb0ba338fd93e3cfaf61ed974
Nikhil Vispute <Vibhuti53>
2021-05-15 23:15:39 +0530

    newton's craddle

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
 const Constraint = Matter.Constraint;
+
 var bob1,bob2,bob3, bob4,bob5, roofObject
 var rope1,rope2,rope3, rope4,rope5;
 var world;
@@ -20,12 +21,11 @@ function setup() {
 
 	roofObject=new roof(400,250,230,20);
 
-	bob1 = new bob(320,575,40);
-	bob2 = new bob(360,575,40);
-	bob3 = new bob(400,575,40);
-	bob4 = new bob(440,575,40);
-	bob5 = new bob(480,575,40);
-	
+	bob1 = new bob(320,475,40);
+	bob2 = new bob(360,475,40);
+	bob3 = new bob(400,475,40);
+	bob4 = new bob(440,475,40);
+	bob5 = new bob(480,475,40);
 	
 	rope1 = new rope(bob1.body,roofObject.body,-80, 0);
 	rope2 = new rope(bob2.body,roofObject.body,-40, 0);
@@ -33,10 +33,7 @@ function setup() {
 	rope4 = new rope(bob4.body,roofObject.body, 40,0);
 	rope5 = new rope(bob5.body, roofObject.body, 80, 0);
 	
-	
 	Engine.run(engine);
-	
-  
 }
 
 
